a991f2955b6ef2af8f4f62015fe9586bb2ebdc68
Aldo Noyola <xarra05@yahoo.com.mx>
2016-10-01 04:33:59 +0000

    Index for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,14 @@ class UsersController < ApplicationController
      
   end
   
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def index
+    @users  = User.all
+  end
+  
   private
   
   def user_params
