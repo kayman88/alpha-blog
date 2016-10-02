7c352f9f5f872eb63e61569c6a896c32e07c2a1a
Aldo Noyola <xarra05@yahoo.com.mx>
2016-10-02 00:09:21 +0000

    Restrict actions from controller side

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update , :show]
+  before_action :require_user,  only: [:edit,:update]
+  before_action :require_same_user, only: [:edit,:update]
+  
   def new
     @user = User.new
   end
@@ -44,5 +47,12 @@ class UsersController < ApplicationController
   
   def set_user
     @user = User.find(params[:id])
+  end
+  
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "You can only edit your own profile"
+      redirect_to root_path
+    end
   end
 end
